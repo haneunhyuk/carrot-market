8049d614a4a133cc9a3bcb3bf13127c7d3220a8c
hehyck <hehyck@openobject.net>
2024-01-24 14:32:56 +0900

    프로젝트 초기 설정 수정

---
+++ carrot-market-project/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="select" class="select">
+      <button type="button">{{ location }}</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  select : String
+})
+
+const location = '중구';
+</script>
+
+<style scoped>
+
+</style>
--- carrot-market-project/src/components/Icon.vue
+++ carrot-market-project/src/components/Icon.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-
+  <div
+    :class="`icon ${props.cover ? 'ui-bg-img' : 'ui-bg-img-contain'}`"
+    :style="{
+      backgroundImage: 'url(' + require(`@/assets/images/${props.src}.png`) + ')',
+      width: props.width,
+      height: props.height,
+    }"
+    @click="$emit('click', true)"
+  >
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  width: {
+    type: String,
+  },
+  height: {
+    type: String,
+  },
+  cover: {
+    type: Boolean,
+    default: true,
+  }
+})
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.ui-bg-img {
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: cover;
+  &-contain {
+    background-size: contain;
+  }
+}
 </style>

53ae2db324173a49eb33f91c825abe6f61c1789c
hp2580 <hp2580@openobject.net>
2024-01-26 17:54:37 +0900

    Revert "Revert "ListContent 수정, content data 수정""
    This reverts commit 16fbb4065163cbc5e75d062c4b3322b18239566f.

--- carrot-market-project/src/views/LifePage.vue
+++ carrot-market-project/src/views/LifePage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <FilterComponont :data="activeData[0]"></FilterComponont>
+    <ListContent  v-for="cont in activeData[0].content" :key="cont.title" :content="cont" :class="path"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import FilterComponont from '@/components/FilterComponont.vue';
+import ListContent from '@/components/ListContent.vue';
 import { baseData } from '@/datas/datas'
 import { ref, watch, defineProps } from 'vue';
 import { useRoute } from 'vue-router';
